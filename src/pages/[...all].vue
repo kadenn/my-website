64b50e478753f447935d7d844fbcb5f67b6fb948
kadenn <k.demirhindi@gmail.com>
2022-08-04 02:00:22 +0300

    added 404 and example pages

---
+++ src/pages/[...all].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <main>
+      <div>
+        <h1>404</h1>
+        <h2>Sorry, we couldn’t find the page you’re looking for</h2>
+        <div class="mt-4">
+          <router-link to="/"> Go back home<span> &rarr;</span> </router-link>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
---
+++ src/pages/example.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Example Page</h1>
+    <p>This is an example page to show you how page routing works.</p>
+  </div>
+</template>
